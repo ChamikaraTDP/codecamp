f61992e35db625cb02fb58e60f95a644792540b5
ChamikaraTDP <pramudithachamikara@gmail.com>
2019-08-03 21:41:04 +0530

    profile design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,36 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="/images/me1.jpg"  class="w-100 rounded-circle"/>
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>codecamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23K</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold">codecamp.org</div>
+            <div>we're global community of millions of people leaning to code together.</div>
+            <div><a href="#">www.codecamp.org</a></div>
+            
         </div>
     </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="/images/me1.jpg" class="w-100"/>
+        </div>
+        <div class="col-4"> 
+            <img src="/images/me1.jpg" class="w-100"/>
+        </div>
+        <div class="col-4"> 
+            <img src="/images/me1.jpg" class="w-100"/>
+        </div>
+    
+    </div>
+
+
 </div>
 @endsection
